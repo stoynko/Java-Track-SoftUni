47279b707264dbd4e8d1ab8c25f241ff5024e169
Yani Stoyanov <167636947+stoynko@users.noreply.github.com>
2025-02-03 18:23:35 +0200

    [03/02/2025]

---
+++ src/C_Advanced/F_DefiningClasses/E07_Google/Car.java
@@ -0,0 +1,17 @@
+package C_Advanced.F_DefiningClasses.E07_Google;
+
+public class Car {
+
+    private String model;
+    private int speed;
+
+    public Car(String model, int speed) {
+        this.model = model;
+        this.speed = speed;
+    }
+
+    @Override
+    public String toString() {
+        return model + " " + speed;
+    }
+}
---
+++ src/C_Advanced/F_DefiningClasses/E07_Google/Pokemon.java
@@ -0,0 +1,17 @@
+package C_Advanced.F_DefiningClasses.E07_Google;
+
+public class Pokemon {
+
+    private String pokemonName;
+    private String pokemonType;
+
+    public Pokemon(String pokemonName, String pokemonType) {
+        this.pokemonName = pokemonName;
+        this.pokemonType = pokemonType;
+    }
+
+    @Override
+    public String toString() {
+        return pokemonName + " " + pokemonType;
+    }
+}
