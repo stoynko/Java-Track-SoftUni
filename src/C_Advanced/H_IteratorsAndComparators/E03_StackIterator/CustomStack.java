b791416b3f380d9d0db35df5aa20729b2c5680d3
Yani Stoyanov <167636947+stoynko@users.noreply.github.com>
2025-02-07 21:37:42 +0200

    [07/02/2025]

---
+++ src/C_Advanced/H_IteratorsAndComparators/E03_StackIterator/CustomStack.java
@@ -0,0 +1,45 @@
+package C_Advanced.H_IteratorsAndComparators.E03_StackIterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class CustomStack implements Iterable<Integer> {
+
+    private List<Integer> data;
+
+    public CustomStack() {
+        this.data = new ArrayList<>();
+    }
+
+    public void push(int element) {
+        this.data.add(element);
+    }
+
+    public void pop() {
+        if (data.isEmpty()) {
+            System.out.println("No elements");
+        } else {
+            data.remove(data.get(data.size() - 1));
+        }
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+
+        return new Iterator<Integer>() {
+
+            int index = data.size() - 1;
+
+            @Override
+            public boolean hasNext() {
+                return index >= 0;
+            }
+
+            @Override
+            public Integer next() {
+                return data.get(index--);
+            }
+        };
+    }
+}
