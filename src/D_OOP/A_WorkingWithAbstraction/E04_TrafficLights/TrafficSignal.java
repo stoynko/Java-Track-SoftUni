dc29896178816b7838005ad362c4ad94ca7d3fd1
Yani Stoyanov <167636947+stoynko@users.noreply.github.com>
2025-02-17 22:57:07 +0200

    [17/02/2025]

---
+++ src/D_OOP/A_WorkingWithAbstraction/E04_TrafficLights/TrafficSignal.java
@@ -0,0 +1,8 @@
+package D_OOP.A_WorkingWithAbstraction.E04_TrafficLights;
+
+public enum TrafficSignal {
+
+    RED,
+    YELLOW,
+    GREEN
+}
---
+++ src/D_OOP/A_WorkingWithAbstraction/E05_JediGalaxy/OOP_Refactoring/Player.java
@@ -0,0 +1,17 @@
+package D_OOP.A_WorkingWithAbstraction.E05_JediGalaxy.OOP_Refactoring;
+
+public class Player {
+
+    public long movePlayer(int playerX, int playerY, Field field) {
+        long starsCollected = 0;
+
+        while (playerX >= 0 && playerY < field.getFieldLength()) {
+            if (field.isWithinField(playerX, playerY)) {
+                starsCollected += field.getValue(playerX, playerY);
+            }
+            playerY++;
+            playerX--;
+        }
+        return starsCollected;
+    }
+}
