84da41b26bd1f61499d6b556d760655194376f76
Yani Stoyanov <167636947+stoynko@users.noreply.github.com>
2025-02-24 22:10:27 +0200

    [24/02/2025]

---
+++ src/D_OOP/C_Inheritance/E02_Zoo/Lizard.java
@@ -0,0 +1,8 @@
+package D_OOP.C_Inheritance.E02_Zoo;
+
+public class Lizard extends Reptile {
+
+    public Lizard(String name) {
+        super(name);
+    }
+}
---
+++ src/D_OOP/C_Inheritance/E03_PlayersAndMonsters/BladeKnight.java
@@ -0,0 +1,8 @@
+package D_OOP.C_Inheritance.E03_PlayersAndMonsters;
+
+public class BladeKnight extends DarkKnight {
+
+    public BladeKnight(String username, int level) {
+        super(username, level);
+    }
+}
