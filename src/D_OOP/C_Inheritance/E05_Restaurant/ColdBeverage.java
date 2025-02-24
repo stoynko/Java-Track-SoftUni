5322c3b52074d5675e155c8f7e4e844f14a3bdc8
Yani Stoyanov <167636947+stoynko@users.noreply.github.com>
2025-02-24 23:25:43 +0200

    [24/02/2025]

---
+++ src/D_OOP/C_Inheritance/E05_Restaurant/ColdBeverage.java
@@ -0,0 +1,10 @@
+package D_OOP.C_Inheritance.E05_Restaurant;
+
+import java.math.*;
+
+public class ColdBeverage extends Beverage {
+
+    public ColdBeverage(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+}
