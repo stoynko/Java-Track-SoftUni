ac6fd3a45f2eb9ed4d3670fbbaca879109c9d27e
Yani Stoyanov <167636947+stoynko@users.noreply.github.com>
2025-02-25 20:09:30 +0200

    [25/02/2025]

---
+++ src/B_Fundamentals/F_ObjectsAndClasses/AE02_RawData/Engine.java
@@ -0,0 +1,15 @@
+package B_Fundamentals.F_ObjectsAndClasses.AE02_RawData;
+
+public class Engine {
+
+    private int engineSpeed;
+    private int enginePower;
+
+    Engine (int engineSpeed, int enginePower) {
+        this.engineSpeed = engineSpeed;
+        this.enginePower = enginePower;
+    }
+    public int getEnginePower() {
+        return enginePower;
+    }
+}
