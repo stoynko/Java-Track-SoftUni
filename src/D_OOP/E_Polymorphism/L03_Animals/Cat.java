b8e9b77451d77e1ce2e91cbc3e18f613ba95e7d7
Yani Stoyanov <167636947+stoynko@users.noreply.github.com>
2025-03-05 22:26:10 +0200

    [05/03/2025]

---
+++ src/D_OOP/E_Polymorphism/L03_Animals/Cat.java
@@ -0,0 +1,13 @@
+package D_OOP.E_Polymorphism.L03_Animals;
+
+public class Cat extends Animal {
+
+    public Cat(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return super.explainSelf() + System.lineSeparator() + "MEEOW";
+    }
+}
---
+++ src/D_OOP/E_Polymorphism/L03_Animals/Main.java
@@ -0,0 +1,19 @@
+package D_OOP.E_Polymorphism.L03_Animals;
+
+public class Main {
+
+/*Create a class Animal, which holds two fields:
+    •	name: String
+    •	favouriteFood: String
+The Animal has one abstract method explainSelf(): String.
+You should add two new classes - Cat and Dog. Override the explainSelf() method by adding concrete animal sound on a new line. (Look at examples below) */
+
+    public static void main(String[] args) {
+
+        Animal cat = new Cat("Oscar", "Whiskas");
+        Animal dog = new Dog("Rocky", "Meat");
+        System.out.println(cat.explainSelf());
+        System.out.println(dog.explainSelf());
+
+    }
+}
