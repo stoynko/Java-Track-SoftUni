a189c4785d54db44fa6f1f2144dce62c9c35e90a
Yani Stoyanov <167636947+stoynko@users.noreply.github.com>
2025-03-07 21:46:17 +0200

    [07/03/2025]

--- src/D_OOP/E_Polymorphism/E02_VehiclesExtension/Car.java
+++ src/D_OOP/E_Polymorphism/E02_VehiclesExtension/Car.java
@@ -1,11 +1,11 @@
-package D_OOP.E_Polymorphism.E01_Vehicles;
+package D_OOP.E_Polymorphism.E02_VehiclesExtension;
 
 public class Car extends Vehicle {
 
     public final static double AIR_CONDITIONER_CONSUMPTION = 0.9;
 
-    public Car(double fuelQuantity, double fuelConsumption) {
-        super(fuelQuantity, fuelConsumption + AIR_CONDITIONER_CONSUMPTION);
+    public Car(double fuelQuantity, double fuelConsumption, double tankCapacity) {
+        super(fuelQuantity, fuelConsumption + AIR_CONDITIONER_CONSUMPTION, tankCapacity);
     }
 
     @Override
