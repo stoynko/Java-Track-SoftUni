6677acf577eb1eaeb3329fe075845047ac890caa
Yani Stoyanov <167636947+stoynko@users.noreply.github.com>
2025-03-08 10:58:26 +0200

    [08/03/2025]

---
+++ src/D_OOP/E_Polymorphism/E04_Calculator/Extensions.java
@@ -0,0 +1,8 @@
+package D_OOP.E_Polymorphism.E04_Calculator;
+
+public class Extensions {
+
+    public static InputInterpreter buildInterpreter(CalculationEngine engine) {
+        return new InputInterpreter(engine);
+    }
+}
---
+++ src/D_OOP/E_Polymorphism/E04_Calculator/Operation.java
@@ -0,0 +1,7 @@
+package D_OOP.E_Polymorphism.E04_Calculator;
+
+public interface Operation {
+    void addOperand(int operand);
+    int getResult();
+    boolean isCompleted();
+}
