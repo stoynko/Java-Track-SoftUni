7672a212b67419a24cebf2e0e39ed26431e82482
Yani Stoyanov <167636947+stoynko@users.noreply.github.com>
2025-03-20 23:57:04 +0200

    [20/03/2025]

---
+++ src/test/java/OOP/UnitTesting_E05_CustomLinkedList/CustomLinkedListTests.java
@@ -0,0 +1,15 @@
+package OOP.UnitTesting_E05_CustomLinkedList;
+
+import org.junit.jupiter.api.*;
+
+/* You are given a data structure that needs to be tested. Use the Java file CustomLinkedList.java and:
+    •	Create Test Classes and Test Methods for all public members that need testing.
+    •	Create tests that ensure all methods, getters and setters work correctly.
+    •	Use the Assertions.assertThrows(ExpectedException.class, () -> {}); for methods that are expected to throw an
+        exception in case wrong input is entered (those tests don’t need to assert messages).
+    •	Give meaningful assert messages for failed tests. */
+
+public class CustomLinkedListTests {
+
+
+}
