9f802c2fbbf27cb22101c57ed45576f740bc5123
Yani Stoyanov <167636947+stoynko@users.noreply.github.com>
2025-04-05 22:48:18 +0300

    [05/04/2025]

--- src/main/java/E_MySQL/A_DatabasesIntroduction/L01_CreateTables.sql
+++ src/main/java/E_MySQL/A_DatabasesIntroduction/L01_CreateTables.sql
@@ -1,17 +1,17 @@
 
-CREATE TABLE employees (
+CREATE TABLE `employees` (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50) NULL,
     last_name  VARCHAR(50) NULL
 );
 
-CREATE TABLE products (
+CREATE TABLE `products` (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     category_id INT NOT NULL
 );
 
-create table categories (
+create table `categories` (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL
 );
---
+++ src/main/java/E_MySQL/A_DatabasesIntroduction/L02_InsertDataInTables.sql
@@ -0,0 +1,4 @@
+INSERT INTO `employees` VALUES
+(1, 'Georgi', 'Georgiev'),
+(2, 'Petar', 'Petrov'),
+(3, 'Ivan', 'Ivanov');
