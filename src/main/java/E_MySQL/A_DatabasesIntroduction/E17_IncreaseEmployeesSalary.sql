5f6bd2e41c319411efc96fc4a4da116637a1ae4b
Yani Stoyanov <167636947+stoynko@users.noreply.github.com>
2025-04-08 20:19:31 +0300

    [08/04/2025]

---
+++ src/main/java/E_MySQL/A_DatabasesIntroduction/E17_IncreaseEmployeesSalary.sql
@@ -0,0 +1,6 @@
+/*Use softuni database and increase the salary of all employees by 10%.
+Then select only salary column from the employees table.
+Submit your query statements as Prepare DB & Run queries. */
+
+UPDATE `employees` SET `salary` = `salary` * 1.1;
+SELECT `salary` FROM `employees`;
