52000f4dc4ceac0da3f79e68df57b10d24ac4ce7
Yani Stoyanov <167636947+stoynko@users.noreply.github.com>
2025-06-12 00:35:38 +0300

    [12/06/2025]

--- src/main/java/F_SpringData/C_IntroductionToHibernate/Main.java
+++ src/main/java/F_SpringData/C_IntroductionToHibernate/Main.java
@@ -3,6 +3,7 @@ import entities.*;
 import jakarta.persistence.*;
 
 import java.io.*;
+import java.math.*;
 import java.util.*;
 
 public class Main {
@@ -34,7 +35,18 @@ public class Main {
         //getAddressWithEmployeeCount(entityManager);
 
         // E08_GetEmployeesWithProject
-        getEmployeeWithProject(entityManager);
+        //getEmployeeWithProject(entityManager);
+
+        // E09_FindTheLatest10Projects
+        //findTheLatestProjects(entityManager);
+
+        // E10_IncreaseSalaries
+        increaseSalaries(entityManager);
+
+        //E11_FindEmployeesByFirstName
+        findEmployeeByFirstName(entityManager);
+
+
 
         entityManager.getTransaction().commit();
         entityManager.close();
@@ -117,10 +129,10 @@ public class Main {
                 address.getEmployees().size()));
     }
 
-    /* Get an employee by his/her id. Print only his/her first name, last name, job title and projects (only their names).
+    /* E08_GetEmployeesWithProject - Get an employee by his/her id. Print only his/her first name, last name, job title and projects (only their names).
        The projects should be ordered by name (ascending). The output should be printed in the format given in the example. */
 
-    private static void getEmployeeWithProject(EntityManager entityManager) throws IOException {
+    private static void getEmployeeWithProject(EntityManager entityManager) {
         Employee employee = entityManager.find(Employee.class, Integer.parseInt(READER.readLine()));
 
         if (employee.getProjects().isEmpty()) {
@@ -129,5 +141,39 @@ public class Main {
 
         System.out.printf("%s %s - %s\n", employee.getFirstName(), employee.getLastName(), employee.getJobTitle());
         employee.getProjects().forEach(project -> System.out.printf("\t%s\n", project.getName()));
+    }
+
+    /* E09_FindTheLatest10Projects - Write a program that prints the last 10 started projects. Print their name, description, start and end date and sort them by name lexicographically. */
+
+    private static void findTheLatestProjects(EntityManager entityManager) {
+        entityManager.createQuery("FROM Project ORDER BY startDate DESC, name ASC", Project.class)
+                .setMaxResults(10).getResultStream().forEach(project -> {
+                    System.out.printf("Project name: %s\n", project.getName());
+                    System.out.printf("\tProject description: %s\n", project.getDescription());
+                    System.out.printf("\tProject start date: %s\n", project.getStartDate());
+                    System.out.printf("\tProject end date: %s\n", project.getEndDate());
+                });
+    }
+
+    /* E10_IncreaseSalaries - Write a program that increases the salaries of all employees, who are in the Engineering, Tool Design, Marketing,
+    or Information Services departments by 12%. Then print the first name, the last name and the salary for the employees,
+    whose salary was increased. */
+
+    private static void increaseSalaries(EntityManager entityManager) {
+        entityManager.createQuery("FROM Employee " +
+                        "WHERE department.name IN('Engineering', 'Tool Design', 'Marketing', 'Information Services')", Employee.class)
+                .getResultStream().forEach(employee -> {
+                    employee.setSalary(employee.getSalary().multiply(BigDecimal.valueOf(1.12)));
+                    entityManager.persist(employee);
+                    System.out.printf("%s %s ($%.2f)\n", employee.getFirstName(), employee.getLastName(), employee.getSalary());
+                });
+    }
+
+    /* E11_FindEmployeesByFirstName - Write a program that finds all employees, whose first name starts with a pattern given as input from the console.
+       Print their first and last names, their job title and salary in the format given in the example below. */
+
+    private static void findEmployeeByFirstName(EntityManager entityManager) throws IOException {
+        String userInput = READER.readLine();
+        entityManager.createQuery("FROM Employee WHERE firstName LIKE(userInput)")
     }
 }
