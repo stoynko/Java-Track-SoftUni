e2f24adba726d1b29d577c4c699ff6a068810a5c
Yani Stoyanov <167636947+stoynko@users.noreply.github.com>
2025-07-08 21:04:50 +0300

    [08/07/2025]

---
+++ 06_Spring_Data/08_Spring_Data_JSON_Processing/E01_Products_Shop/src/main/java/E01_Products_Shop/E01_Products_Shop.java
@@ -0,0 +1,12 @@
+package E01_Products_Shop;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class E01_Products_Shop {
+
+    public static void main(String[] args) {
+        SpringApplication.run(E01_Products_Shop.class, args);
+    }
+}
---
+++ 06_Spring_Data/08_Spring_Data_JSON_Processing/E01_Products_Shop/src/test/java/E01_Products_Shop/E01ProductsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package E01_Products_Shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E01ProductsShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
