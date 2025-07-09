d13ba19bae3ffb8618a2b6333be26c891d39bb9d
Yani Stoyanov <167636947+stoynko@users.noreply.github.com>
2025-07-09 15:27:34 +0300

    [09/07/2025]

--- 06_Spring_Data/08_Spring_Data_JSON_Processing/E01_Products_Shop/src/main/java/E01_Products_Shop/controllers/CommandRunner.java
+++ 06_Spring_Data/08_Spring_Data_JSON_Processing/E01_Products_Shop/src/main/java/E01_Products_Shop/controllers/CommandRunner.java
@@ -2,7 +2,9 @@ package E01_Products_Shop.controllers;
 
 import E01_Products_Shop.service.*;
 import org.springframework.boot.*;
+import org.springframework.stereotype.*;
 
+@Component
 public class CommandRunner implements CommandLineRunner {
 
     private CategoryService categoryService;
--- 06_Spring_Data/08_Spring_Data_JSON_Processing/E01_Products_Shop/src/main/java/E01_Products_Shop/service/implementations/CategoryServiceImpl.java
+++ 06_Spring_Data/08_Spring_Data_JSON_Processing/E01_Products_Shop/src/main/java/E01_Products_Shop/service/implementations/CategoryServiceImpl.java
@@ -3,6 +3,8 @@ package E01_Products_Shop.service.implementations;
 import E01_Products_Shop.data.*;
 import E01_Products_Shop.repositories.*;
 import E01_Products_Shop.service.*;
+import E01_Products_Shop.service.dtos.*;
+import E01_Products_Shop.service.utilities.*;
 import com.fasterxml.jackson.core.exc.*;
 import com.fasterxml.jackson.core.type.*;
 import com.fasterxml.jackson.databind.*;
@@ -10,6 +12,7 @@ import org.modelmapper.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.core.io.*;
 import org.springframework.stereotype.*;
+import org.springframework.validation.*;
 
 import java.io.*;
 import java.util.*;
@@ -20,22 +23,29 @@ public class CategoryServiceImpl implements CategoryService {
     private static CategoryRepository categoriesRepository;
     private static ModelMapper modelMapper;
     private static ObjectMapper objectMapper;
-    private static final String CATEGORIES_JSON_PATH = "src/main/resources/categories.json";
+    private static final String CATEGORIES_JSON_PATH = "json/categories.json";
+    private final ValidatorUtil validatorUtil;
 
     @Autowired
-    public CategoryServiceImpl(CategoryRepository categoriesRepository, ModelMapper modelMapper, ObjectMapper objectMapper) {
+    public CategoryServiceImpl(CategoryRepository categoriesRepository, ModelMapper modelMapper, ObjectMapper objectMapper, ValidatorUtil validatorUtil) {
         this.categoriesRepository = categoriesRepository;
         this.modelMapper = modelMapper;
         this.objectMapper = objectMapper;
+        this.validatorUtil = validatorUtil;
     }
 
     @Override
     public void importData() {
         try {
             InputStream inputStream = new ClassPathResource(CATEGORIES_JSON_PATH).getInputStream();
-            List<Category> categories = objectMapper.readValue(inputStream, new TypeReference<List<Category>>(){});
-            System.out.println();
-
+            Set<ImportCategoryDTO> categories = objectMapper.readValue(inputStream, new TypeReference<Set<ImportCategoryDTO>>() {});
+            for (ImportCategoryDTO categoryDTO : categories) {
+                if (!validatorUtil.isValid(categoryDTO)) {
+                    System.out.println(validatorUtil.getViolations(categoryDTO));
+                    continue;
+                }
+             categoriesRepository.saveAndFlush(modelMapper.map(categoryDTO, Category.class));
+            }
         } catch (IOException ex) {
             throw new RuntimeException("Failed to load or parse JSON file: " + CATEGORIES_JSON_PATH, ex);
         }
